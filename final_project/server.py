1432919179f4c87639180909c5bd57400b04d2da
ritikaj23 <ritikaj@skillup.online>
2023-06-23 00:56:33 -0400

    modified requirements.txt

--- final_project/server.py
+++ final_project/server.py
@@ -1,5 +1,6 @@
+from flask import Flask, render_template,request
 from machinetranslation import translator
-from flask import Flask, render_template, request
+
 import json
 
 app = Flask("Web Translator")
